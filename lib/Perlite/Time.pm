6049d93f348136051f898057fdd1f03a535707a9
Timothy Totten <2009@huri.net>
2010-08-30 15:42:30 -0700

    Removed Perlite. Moving it into it's own repository.
    This project, ww6, is currently on hiatus, and will most likely end
    up being largely rewritten.

--- lib/Perlite/Time.pm
+++
@@ -1,9 +0,0 @@
-module Perlite::Time;
-
-use DateTime::Utils;
-
-sub rfc2822(DateTime $dt=DateTime.now) is export(:DEFAULT) {
-    ## TODO: Put in %z when the patch is applied.
-    strftime('%a, %d %b %Y %T %z', $dt);
-}
-
