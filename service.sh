8efc97459ab9970f799f0001b4f9604de1c0f817
Timothy Totten <2009@huri.net>
2010-09-01 16:04:09 -0700

    Updated service script

--- service.sh
+++ service.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
+die() {
+  echo "$@"
+  exit 1
+}
+
+usage() {
+  die "usage: $0 (start|stop|forward|daemon|stop-forward|stop-daemon) <configfile>"
+}
+
+[ $# -lt 2 ] && usage
+
+## Include configuration file.
+if [ -f $2 ]; then
+  . $2
+else
+  die "invalid configuration file specified"
+fi
+
+[ -z "$REMOTE_SERVER" ] && die "no REMOTE_SERVER defined"
+[ -z "$DAEMON_USER" ] && die "no DAEMON_USER defined"
+[ -z "$WEB_APP" ] && die "no WEB_APP defined"
+
 case "$1" in
     forward)
         echo -n "Starting SSH forward... "
-        ssh -R 8118:localhost:8118 -f -N huri.net
+        ssh -R 8118:localhost:8118 -f -N $REMOTE_SERVER
         ps x | grep "ssh -R 8118" | grep -v 'grep' | awk '{ print $1 }' > forward.pid
         echo "done."
     ;;
     daemon)
-        sudo -u nobody ./apps/default.scgi &
+        sudo -u $DAEMON_USER ./apps/$WEB_APP.scgi &
         echo $! > daemon.pid
     ;;
     start)
@@ -37,7 +59,7 @@ case "$1" in
         $0 start
     ;;
     *)
-        echo "usage: $0 (start|stop|forward|daemon|stop-forward|stop-daemon)"
+        usage
     ;;
 esac
 
