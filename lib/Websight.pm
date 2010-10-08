02d62ea1c9d619bf30d9953ba3edbf36b4e1f033
Timothy Totten <2009@huri.net>
2010-10-08 16:50:22 -0700

    Changed Websight::Petal to use Flower's load-modifiers() method.

--- lib/Websight.pm
+++ lib/Websight.pm
@@ -8,8 +8,8 @@ use Hash::Has;
 has $.parent is rw;
 has $.namespace is rw;
 
-method getConfig (:$type) {
-    return hash-has($.parent.metadata, $.namespace, :type($type), :return);
+method getConfig (:$type, :$default) {
+    return hash-has($.parent.metadata, $.namespace, :$type, :$default :return);
 }
 
 method saveConfig ($config) {
